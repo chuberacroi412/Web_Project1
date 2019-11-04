30869cee20014b79f8fdff4db7ec0823f8fc6c9f
chuberacroi412 <37958877+chuberacroi412@users.noreply.github.com>
2019-11-04 20:34:32 +0700

    Add files via upload

--- Client/routes/index.js
+++ Client/routes/index.js
@@ -6,72 +6,72 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/index', (req, res, next) => {
+router.get('/index.html', (req, res, next) => {
   res.render('index');
 });
 
-router.get('/about', function(req, res, next) {
+router.get('/about.html', function(req, res, next) {
   res.render('about');
 }); 
 
-router.get('/cart', (req, res, next)=>{
+router.get('/cart.html', (req, res, next)=>{
   res.render('cart');
 });
 
-router.get('/checkout',(req, res, next)=>{
+router.get('/checkout.html',(req, res, next)=>{
   res.render('checkout');
 });
 
-router.get('/contact-us',(req, res, next)=>{
+router.get('/contact-us.html',(req, res, next)=>{
   res.render('contact-us');
 });
 
-router.get('/my-account',(req, res, next)=>{
+router.get('/my-account.html',(req, res, next)=>{
   res.render('my-account');
 });
 
-router.get('/service',(req, res, next)=>{
+router.get('/service.html',(req, res, next)=>{
   res.render('service');
 });
 
-router.get('/shop-detail',(req, res, next)=>{
+router.get('/shop-detail.html',(req, res, next)=>{
   res.render('shop-detail');
 });
 
-router.get('/shop',(req, res, next)=>{
+router.get('/shop.html',(req, res, next)=>{
   res.render('shop');
 });
 
-router.get('/wishlist',(req, res, next)=>{
+router.get('/wishlist.html',(req, res, next)=>{
   res.render('wishlist');
 });
 
-router.get('/register',(req, res, next)=>{
+router.get('/register.html',(req, res, next)=>{
   res.render('register');
 });
 
 
-router.get('/login',(req, res, next)=>{
+router.get('/login.html',(req, res, next)=>{
   res.render('login');
 });
 
-  router.get('/searchResult',(req, res, next)=>{
+  router.get('/searchResult.html',(req, res, next)=>{
     res.render('searchResult');
 });
 
-router.get('/orders-history',(req, res, next)=>{
+router.get('/orders-history.html',(req, res, next)=>{
   res.render('orders-history');
 });
 
-router.get('/update-account',(req, res, next)=>{
+router.get('/update-account.html',(req, res, next)=>{
   res.render('update-account');
 });
 
-router.get('/update-accept',(req, res, next)=>{
+router.get('/update-accept.html',(req, res, next)=>{
   res.render('update-accept');
 });
 
-router.get('/forget-password',(req, res, next)=>{
+router.get('/forget-password.html',(req, res, next)=>{
   res.render('forget-password');
 });
 
